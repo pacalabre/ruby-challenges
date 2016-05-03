644b7efcffe734ea69cf1f8915f6779ffafd79c0
Paul <pacalabre@gmail.com>
2016-05-02 17:42:54 -0700

    first part of calculator

--- ruby challenges.rb
+++ ruby challenges.rb
@@ -1,1 +1,22 @@
+class Calculator
+
+  def get_operator
+   puts "What calculation would you like to do? (add, sub, mult, div)"
+   operator = gets.chomp
+  end
+
+  def get_first
+    puts "what is the first number?"
+    first = gets
+  end
+
+  def get_second
+    puts "what is the second number?"
+    second = gets
+  end
+
+end
+
+calc = Calculator.new
+calc.get_operator
 
