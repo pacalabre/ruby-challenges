edffa1b43b5a93e4911842f653874893bfddf06e
Paul <pacalabre@gmail.com>
2016-05-02 18:57:38 -0700

    calc done

--- ruby challenges.rb
+++ ruby challenges.rb
@@ -1,22 +1,64 @@
 class Calculator
 
-  def get_operator
-   puts "What calculation would you like to do? (add, sub, mult, div)"
-   operator = gets.chomp
+  attr_accessor :operator, :a, :b
+
+  def initialize(operator, a, b)
+    @operator = operator
+    @a = a
+    @b = b
+    @result=0
   end
 
-  def get_first
-    puts "what is the first number?"
-    first = gets
+  def calculate (operator,a,b)
+
+    def add(a,b)
+      @result = a + b
   end
 
-  def get_second
-    puts "what is the second number?"
-    second = gets
-  end
+  def sub(a,b)
+      @result = a-b
+    end
 
+    def mult(a,b)
+      @result = a*b
+    end
+
+  def div(a,b)
+   @result = a/b
+    end
+
+    if @operator == "add"
+      add(a,b)
+      return @result
+    end
+
+    if @operator == "sub"
+      sub(a,b)
+      return @result
+    end
+
+    if @operator == "mult"
+      mult(a,b)
+      return @result
+    end
+
+    if @operator == "div"
+      div(a,b)
+      return @result
+    end
+  end
 end
 
-calc = Calculator.new
-calc.get_operator
+puts "what operator would you like to use? (add,sub,mult,div)"
+operator = gets.chomp
 
+puts "what is the first number?"
+a = gets.chomp.to_f
+
+puts "what is the second number?"
+b = gets.chomp.to_f
+
+
+
+calc = Calculator.new(operator,a,b)
+puts calc.calculate(operator,a,b)
